c6de9a7a1d6e3944676740d9b8accea85615af48
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-11-05 15:30:01 -0500

    styles for landing page

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,15 +1,27 @@
 class Api::V1::AppointmentsController < ApiController
 
-def index
+  def index
+    render json: {
+      appointments: Appointment.all
+    }
+  end
 
-  render json: {
-    appointments: Appointment.all
-  }
-end
+  def create
+binding.pry
+    appointment = Appointment.new(appointment_params)
+    if appointment.save
+      render json:{
+        appointments: Appointment.all
+      }
+    else
+      render json:{status:"error"}
+    end
+  end
 
+  private
 
-def create
-end
-
+  def appointment_params
+    params.require(:appointment).permit(:start,:end,:value,:uid)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :personal_datapoints, only: [:create]
       resources :appointments,
-      only: [:index]
+      only: [:index, :create]
     end
   end
 end
